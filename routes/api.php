85a18e33079dad60095f501a629061b0b9a60a2a
Plutonic3000 <vit_post@mail.ru>
2021-01-12 18:00:04 +0300

    Back API Routes Corrections

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,31 @@
 
 use App\Http\Controllers\API\PostController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+Route::group(['prefix' => 'auth'], function() {
+    Route::post('register', '\App\Http\Controllers\API\APIController@register');
+    Route::post('login', '\App\Http\Controllers\API\APIController@login');
+    Route::get('login', '\App\Http\Controllers\API\APIController@login')->name('login');
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::get('logout', '\App\Http\Controllers\API\APIController@logout');
+    });
+});
+
+Route::group(['prefix' => 'account'], function() {
+    Route::middleware('auth:api')->get('profile', function (Request $request) {
+        return $request->user();
+    })->name('profile');
+});
+
+Route::group([
+    'prefix' => 'admin',
+    'middleware' => ['auth:api', 'scope:do_anything'],
+], function () {
+    Route::apiResource('posts', '\App\Http\Controllers\API\PostController');
+    Route::get('blog', function (Request $request) {
+        return $request->user();
+    });
+});
 
 Route::apiResource('posts', PostController::class);
